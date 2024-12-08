8536488d2b06c090caab504f239678b6b6601ae6
Yanina Morales <x23437812@student.ncirl.ie>
2024-12-08 02:11:00 +0000

    Update formValidation.js

--- js/formValidation.js
+++ js/formValidation.js
@@ -1,5 +1,16 @@
 /* Validation form */
 
+//CHECKBOX
+
+const checkbox = document.querySelector('.my-form input[type="checkbox"]');
+const btns = document.querySelectorAll(".my-form button");
+checkbox.addEventListener("change", function() {
+  const checked = this.checked;
+  for (const btn of btns) {
+    checked ? (btn.disabled = false) : (btn.disabled = true);
+  }
+});
+
 const form = document.getElementById("contactUs"),
       email = document.getElementById("mail"),
       subject = document.getElementById("subject"),
